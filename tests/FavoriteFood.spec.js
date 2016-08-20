3b7794a5a43b90953388570ba345910cc3289e63
reichardn <reichardn@gmail.com>
2016-08-19 20:50:12 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,12 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+  it('should filter the foods', function () {
+      var mockedList = [{name: 'dave', favoriteFood: 'pizza'}, {name: 'peter', favoriteFood: 'burger'}]
+      var results = $filter('favoriteFood')(mockedList, 'pizza');
+      expect(results.length).toBe(1);
+      expect(results[0].name).toBe('dave');
+  });
+
 });
+
